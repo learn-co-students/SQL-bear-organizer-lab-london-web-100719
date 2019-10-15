c857b5c86affbff6f5fb583dbdfab03f27ec02dd
flow1981 <florian.stagliano@gmx.de>
2019-10-15 16:50:33 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES
+('Mr. Chocolate',   10, 'male',     'brown', 'active',     TRUE),
+('Rowdy',            0, 'male',     'brown', 'friendly',   TRUE),
+('Tabitha',         12, 'female',   'black', 'active',     FALSE),
+('Sergeant Brown',   5, 'female',   'grey',  'active',     TRUE),
+('Melissa',          7, 'female',   'brown', 'active',     TRUE),
+('Grinch',          15, 'male',     'grey',  'active',     TRUE),
+('Wendy',           3, 'male',      'grey',  'active',     FALSE),
+(NULL,           3, 'male',   'grey',  'active',     TRUE)
+;
