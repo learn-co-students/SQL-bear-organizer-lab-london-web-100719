89b0355041e4debd5d2a087c65ca895a4558291d
geewey <geewey@gmail.com>
2019-10-15 17:08:57 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 15, "male", "brown", "happy", 0),
+    ("Rowdy", 5, "male", "brown", NULL, 0),
+    ("Tabitha", NULL, "female", NULL, "happy", 1),
+    ("Sergeant Brown", 33, "female", "black", "hungry", 0),
+    ("Melissa", 29, NULL, "brown", "aggressive", 1),
+    ("Grinch", 3, "female", "gray", "happy", NULL),
+    ("Wendy", 12, "male", "black", "hungry", 0),
+    (NULL, 8, NULL, NULL, "full", NULL);
